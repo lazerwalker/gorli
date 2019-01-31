11df6e45e0a28b2cf77dfca62fec30bb8dff3a02
Mike Lazer-Walker <michael@lazerwalker.com>
2019-01-31 13:12:13 +0100

    Updates

---
+++ precache-manifest.a517751d800b0b591291576dee2e9a4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "aa86b7178e1ed559a958",
+    "url": "./static/js/main.aa86b717.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "aa86b7178e1ed559a958",
+    "url": "./static/css/main.3acd7c3f.chunk.css"
+  },
+  {
+    "revision": "ea01beea645dd99be02be034b329bfe0",
+    "url": "./index.html"
+  }
+];
